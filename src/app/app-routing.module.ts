620e648ab44154c40f0dd940663ec3ad03c6f00e
BiriK121 <isaac_azua@hotmail.com>
2023-09-20 16:55:49 -0600

    commponents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {INICIOComponent} from '../app/Component/pages/inicio/inicio.component'
+import {GALERIAComponent} from '../app/Component/pages/galeria/galeria.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Inicio',component:INICIOComponent},
+   {path:'Galeria',component:GALERIAComponent},
+  {path:'**',component:INICIOComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NAVBARComponent } from './Component/navbar/navbar.component';
 import { PAGESComponent } from './Component/pages/pages.component';
-import { DATOSComponent } from './Component/PAGES/datos/datos.component';
-import { GALERIAComponent } from './Component/PAGES/galeria/galeria.component';
-import { INICIOComponent } from './Component/PAGES/inicio/inicio.component';
-import { MAPAComponent } from './Component/PAGES/mapa/mapa.component';
+import { DATOSComponent } from './Component/pages/datos/datos.component';
+import { GALERIAComponent } from './Component/pages/galeria/galeria.component';
+import { INICIOComponent } from './Component/pages/inicio/inicio.component';
+import { MAPAComponent } from './Component/pages/mapa/mapa.component';
 import { FooterComponent } from './Component/pages/footer/footer.component';
 
 @NgModule({
